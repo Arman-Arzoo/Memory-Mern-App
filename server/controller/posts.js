90d40bebbf49cb844eeebb94ea6a490a779412f5
Arman Ali <toplinegeeks@gmail.com>
2021-08-25 12:02:36 +0500

    add delete router and controller

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,5 +1,7 @@
+import mongoose  from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
+// get all the posts
 export const getPosts = async (req,res)=>{
    try {  
     const PostMessages = await PostMessage.find();
@@ -10,6 +12,7 @@ export const getPosts = async (req,res)=>{
    }
 }
 
+// create a new post
 export const createPosts = async(req,res)=>{
     const { title, message, selectedFile, creator, tags } = req.body;
     const newPosts = new PostMessage({ title, message, selectedFile, creator, tags })
@@ -18,5 +21,22 @@ export const createPosts = async(req,res)=>{
         res.status(201).json(newPosts);     
     } catch (error) {
         res.status(409).json({message:error.message});
+    }
+}
+
+// update the post with id
+
+export const updatePost = async ( req, res) => {
+
+    const post = req.body;
+    const {id:_id} = req.params
+     if(!mongoose.type.ObjectId.isValid(_id)) {
+         return res.status(404).send("no account with that id ")
+     }
+    try {
+        const updatePosts = await PostMessage.findByIdAndUpdate(_id, post, {new:true});
+        res.json(updatePosts)
+    } catch (error) {
+        res.status(404).json({message:error.message})
     }
 }
