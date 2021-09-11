ba83c0268d6449e3d40964499144c028b0e008e3
Arman Ali <51259303+Arman-Arzoo@users.noreply.github.com>
2021-09-11 20:27:40 +0500

    create a new user model

---
+++ server/src/Database/models/user.js
@@ -0,0 +1,13 @@
+import mongoose  from "mongoose";
+
+const userSchema = mongoose.Schema({
+    firstName: String,
+    lastName:String,
+    email:String,
+    password:String,
+   
+   
+}timeStamp: true);
+
+const User = mongoose.model('User', userSchema);
+export default User;
