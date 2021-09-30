1ed8a0797fbe7591d27864a80614f4c528e7bc21
Arman Ali <51259303+Arman-Arzoo@users.noreply.github.com>
2021-09-30 21:02:28 +0500

    Update postMessage.js
    add comment instance in mongoose schema

--- server/src/Database/models/postMessage.js
+++ server/src/Database/models/postMessage.js
@@ -15,6 +15,10 @@ const postSchema = new mongoose.Schema({
     type: [String],
     default: [],
   },
+  comments: {
+    type: [String],
+    default: [],
+  },
 
   createdAt: {
     type: Date,
