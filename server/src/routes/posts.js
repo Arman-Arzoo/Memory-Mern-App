97a0abe70bfea047d0829cf4f327c76f86be39f0
Arman Ali <51259303+Arman-Arzoo@users.noreply.github.com>
2021-10-01 10:23:27 +0500

    Update posts.js

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,11 +1,19 @@
-import express from 'express';
-import { auth } from '../middleware/auth.js';
-import {getPosts, createPosts, updatePost,deleteUser,likePost} from '../controller/posts.js'
+import express from "express";
+import { auth } from "../middleware/auth.js";
+import {
+  getPosts,
+  createPosts,
+  updatePost,
+  deleteUser,
+  likePost,
+  commentPost,
+} from "../controller/posts.js";
 const router = express.Router();
 
-router.get('/',getPosts);
-router.post('/',auth,createPosts);
-router.patch('/:id',auth,updatePost)
-router.delete('/:id',auth,deleteUser)
-router.patch('/:id/likePost', auth, likePost);
+router.get("/", getPosts);
+router.post("/", auth, createPosts);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deleteUser);
+router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", auth, commentPost);
 export default router;
