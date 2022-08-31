fb06f87928e4020ec278fdabcbd5999cdef63489
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-31 12:39:55 +0500

    create new file for store

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,7 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { reducers } from "./reducer";
-
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+import store from "./store/store";
 
 ReactDOM.render(
   <Provider store={store}>
